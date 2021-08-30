f73f6a284ec9cb4f2037f30cd582b7a46af9ed3c
natalii2101 <89312282199n@gmail.com>
2021-08-30 21:31:29 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayNew = [];
+    if (str.length % 2 === 1) { return false; }
+    for (let i = 0; i <= str.length - 1; i++) {
+        let isFound = false,
+            item = 0;
+        while (item < bracketsConfig.length) {
+            if (str[i] === bracketsConfig[item][0]) {
+                isFound = true;
+                if (bracketsConfig[item][0] === bracketsConfig[item][1] &&
+                    arrayNew[arrayNew.length - 1] === bracketsConfig[item][0]) {
+                        arrayNew.pop();
+                        break;
+                }
+                arrayNew.push(bracketsConfig[item][1]);
+                break;
+            }
+            item++;
+        }
+        if (isFound === false) {
+            if (arrayNew.length === 0 || str[i] != arrayNew[arrayNew.length - 1]) { return false; }
+            arrayNew.pop();
+        }
+    }
+    return arrayNew.length === 0 ? true : false;
 }
